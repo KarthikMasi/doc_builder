63b793595d8692e26e5c6f95152fe8f7db69d62a
ramadak <ramadak@masi-2.ds.vanderbilt.edu>
2018-04-02 19:35:43 -0500

    Removed dlb

--- main.py
+++ main.py
@@ -3,68 +3,121 @@
 
 import redcap, os
 from docx import Document
-from requests import post
-import dlb
-
-#change API key(second field in redcap.project) for new document generation
-project = redcap.Project('https://redcap.vanderbilt.edu/api/', 'F44F337CD4C1B009316F0B2C3E4B913F')
-
-fields ='dl_first_name','dl_last_name','dl_dob','dl_age','dl_doa_45c','dl_sex', \
-        'dl_wj_lwid_ss_grade','dl_wj_lwid_pile_grade','dl_wj_wa_ss_grade',\
-        'dl_wj_wa_pile_grade','dl_wj_pc_ss_grade','dl_wj_pc_pile_grade', \
-        'dl_wj_oc_ss_grade','dl_wj_oc_pile_grade','dl_towre_swe_ss_grade',\
-        'dl_towre_swe_pile_grade','dl_towre_pde_ss_grade','dl_towre_pde_pile_grade',\
-        'dl_towre_twe_ss_grade','dl_towre_twe_pile_grade','dl_towk_exp_ss',\
-        'dl_towk_rec_ss','dl_ctopp_el_ss','dl_ctopp_nr_ss','dl_wiscds_ss'
+import argparse
 
 
-def document_template_access(doc_path, doc_name):
+def redcap_project_access(API_KEY):
+    """
+    Gets access to redcap form
+    :param API_KEY:String obtained as argparse argument object
+    :return: redcap Project object
+    """
+    project = redcap.Project('https://redcap.vanderbilt.edu/api/', API_KEY)
+    return project
+
+def participant_filter(project,OPTIONS):
+    """
+    Filter participant data from export data. 
+    :param data: json object containing list of  dicts
+    :param participant_id: parsed from user
+    no return
+    """
+    data = redcap_field_fetch(project)
+    participant_id = splitter(OPTIONS.participant_id)
+    for participant_data in data:
+        for participant in participant_id:
+            if(participant in participant_data.values()):
+                document = document_template_open(OPTIONS.file) 
+                doc = replace_field(participant_data,document,participant,OPTIONS)
+
+def replace_field(data,document,participant,OPTIONS):
+    """
+    Loops through the document and replaces the field string with
+    appropriate values from redcap form
+    :param project: redcap.Project Object
+    :param field: string object to search for in template document
+    :param replacement: string of redcap field variable name
+    :return: docx.Document object
+    """
+    replacements = splitter(OPTIONS.replacement)
+    for replacement in replacements:
+        for para in document.paragraphs:
+            if "sex" in replacement:
+                para.text = para.text.replace('his/her', 'her') if data.get(replacement)=='0' \
+                                    else para.text.replace('his/her', 'his')
+            else:
+                para.text = para.text.replace(replacement,data.get(replacement)) if not \
+                    data.get(replacement)==None else para.text.replace(replacement,'NA')
+        doc = replace_in_tables(data, document, replacement)
+        save_doc(doc,OPTIONS.destination,participant)
+    return document
+
+def splitter(option):
+    """
+    Splits participant id's by ','
+    :param participant_id: args parsed from user
+    :return: list containing participants
+    """
+    if not option:
+        return None 
+    else:
+        return option.split(',')
+
+def save_doc(document,destination,participant_id):
+    """
+    Saves document with replaced fields with participant id as filename
+    :param document: docx.document object
+    :param destination: args parsed from user
+    :param participant_id: participant id of user
+    no return
+    """
+    filename = destination+'/'+participant_id+'.docx'
+    document.save(filename)
+
+def replace_in_tables(data,document,replacement):
+    """
+    Loops through tables in the document and replaces the field string with
+    the replacement data from redcap form
+    :param project: redcap.Project Object
+    :param field: string object to search for in template document
+    :param replacement: string of redcap field variable name
+    :return: docx.Document object
+    """
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for para in cell.paragraphs:
+                    para.text = para.text.replace(replacement,data.get(replacement)) if not data.get(replacement)==None \
+                                            else para.text.replace(replacement,'NA')
+    return document
+
+def document_template_open(file_path):
     """
     Opens the document template used for document generation
     :param doc_name: filename of template document
     :param doc_path: to template document
     :return: True if file exists
     """
-    file = os.path.join(doc_path,doc_name)
-    if os.path.exists(file):
-        document = Document(file)
+    if os.path.exists(file_path):
+        document = Document(file_path)
         return document
     else:
         print ("""Template file doesn't exist: Check filepath and filename provided
         Hint:Also check that suffix of filename is provided eg:docx in filename.docx""")
 
-def redcap_field_fetch(fields):
+def redcap_field_fetch(project):
     """
     Exports data from the redcap form associated with the API Key. 
     Only the fields mentioned are pulled.
+    :param project: redcap.Project object
     :param fields: fields from the redcap form that are
                    required in the document. By default, it is the global variable 
                    where the fields are mentioned. In case of new document, just change the 
                    fields accordingly
     :return:json object with a list of dicts. Each participant info is a dict
     """
-    data = project.export_records(fields=fields, format = 'json')
+    data = project.export_records(format='json')
     return data
-
-def replace_demography(document, data):
-    """
-    Replaces the demographic information in page 1(Participant ID, dob, age, date
-    :param document: docx.Document object. Current copy of document in process for new ID
-    :param data: dict structure passed from json object. Contains key value pairs
-    :return document: docx.Document object
-    """
-    for para in document.paragraphs:
-        para.text = para.text.replace('[dl_age]',data.get('dl_age')) if not data.get('dl_age')==None \
-                else para.text.replace('[dl_age]','NA') #age
-        para.text = para.text.replace('[dl_dob]',data.get('dl_dob')) if not data.get('dl_dob')==None \
-                else para.text.replace('[dl_dob]','NA') #DOB
-        para.text = para.text.replace('[dl_date]',data.get('dl_doa_45c')) if not data.get('dl_doa_45c')==None \
-                else para.text.replace('[dl_date]','NA') #date
-        para.text = para.text.replace('[dl_first_name]',data.get('dl_first_name')) if not data.get('dl_first_name')==None \
-    else para.text.replace('[dl_first_name] [dl_last_name]','ID: '+data.get('dl_participant_id')) #firstname
-        para.text = para.text.replace('[dl_last_name]',data.get('dl_last_name')) if not data.get('dl_last_name')==None \
-                else para.text.replace('[dl_first_name] [dl_last_name]',data.get('dl_participant_id')) #lastname
-    return document
 
 def replace_sex(document, data):
     """
@@ -79,70 +132,35 @@ def replace_sex(document, data):
                     else para.text.replace('his/her', 'his')
     return document
 
-
-def replace_table_contents(document, data):
+def add_to_parser():
     """
-    Replaces the text in WJ-IV tables with actual values
-    :param document: docx.Document object
-    :param data: dict structure derived from json list. Contains key value pairs
-    :return document: docx.Document object
+    Method to add arguments to default parser for Document generator.
+
+    :return: parser object with new arguments
     """
-    for table in document.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                for para in cell.paragraphs:
-                    #WJIV
-                    para.text = para.text.replace('dl_wj_lwid_ss_grade',data.get('dl_wj_lwid_ss_grade')) if not data.get('dl_wj_lwid_ss_grade')==None \
-                            else para.text.replace('dl_wj_lwid_ss_grade','NA') #lwid_ss_grade
-                    para.text = para.text.replace('dl_wj_lwid_pile_grade',data.get('dl_wj_lwid_pile_grade')) if not data.get('dl_wj_lwid_pile_grade')==None \
-                            else para.text.replace('dl_wj_lwid_pile_grade','NA') #lwid_pile_grade
-                    para.text = para.text.replace('dl_wj_wa_ss_grade',data.get('dl_wj_wa_ss_grade')) if not data.get('dl_wj_wa_ss_grade')==None \
-                            else para.text.replace('dl_wj_wa_ss_grade','NA') #wa_ss_grade
-                    para.text = para.text.replace('dl_wj_wa_pile_grade',data.get('dl_wj_wa_pile_grade')) if not data.get('dl_wj_wa_pile_grade')==None \
-                            else para.text.replace('dl_wj_wa_pile_grade','NA') #wa_pile_grade
-                    para.text = para.text.replace('dl_wj_pc_ss_grade',data.get('dl_wj_pc_ss_grade')) if not data.get('dl_wj_pc_ss_grade')==None \
-                            else para.text.replace('dl_wj_pc_ss_grade','NA') #pc_ss_grade
-                    para.text = para.text.replace('dl_wj_pc_pile_grade',data.get('dl_wj_pc_pile_grade')) if not data.get('dl_wj_pc_pile_grade')==None \
-                            else para.text.replace('dl_wj_pc_pile_grade','NA') #pc_pile_grade
-                    para.text = para.text.replace('dl_wj_oc_pile_grade',data.get('dl_wj_oc_pile_grade')) if not data.get('dl_wj_oc_pile_grade')==None \
-                            else para.text.replace('dl_wj_oc_pile_grade','NA') #oc_pile_grade
-                    para.text = para.text.replace('dl_wj_oc_ss_grade',data.get('dl_wj_oc_ss_grade')) if not data.get('dl_wj_oc_ss_grade')==None \
-                            else para.text.replace('dl_wj_oc_ss_grade','NA') #oc_ss_grade
-                    #TOWRE-2
-                    para.text = para.text.replace('dl_towre_swe_ss_grade',data.get('dl_towre_swe_ss_grade')) if not data.get('dl_towre_swe_ss_grade')==None \
-                            else para.text.replace('dl_towre_swe_ss_grade','NA') #swe_ss_grade
-                    para.text = para.text.replace('dl_towre_swe_pile_grade',data.get('dl_towre_swe_pile_grade')) if not data.get('dl_towre_swe_pile_grade')==None \
-                            else para.text.replace('dl_towre_swe_pile_grade','NA') #swe_pile_grade
-                    para.text = para.text.replace('dl_towre_pde_ss_grade',data.get('dl_towre_pde_ss_grade')) if not data.get('dl_towre_pde_ss_grade')==None \
-                            else para.text.replace('dl_towre_pde_ss_grade','NA') #pde_ss_grade
-                    para.text = para.text.replace('dl_towre_pde_pile_grade',data.get('dl_towre_pde_pile_grade')) if not data.get('dl_towre_pde_pile_grade')==None \
-                            else para.text.replace('dl_towre_pde_pile_grade','NA') #pde_pile_grade
-                    para.text = para.text.replace('dl_towre_twe_ss_grade',data.get('dl_towre_twe_ss_grade')) if not data.get('dl_towre_twe_ss_grade')==None \
-                            else para.text.replace('dl_towre_twe_ss_grade','NA') #twe_ss_grade
-                    para.text = para.text.replace('dl_towre_twe_pile_grade',data.get('dl_towre_twe_pile_grade')) if not data.get('dl_towre_twe_pile_grade')==None \
-                            else para.text.replace('dl_towre_twe_pile_grade','NA') #twe_pile_grade
-                    #TOWK
-                    para.text = para.text.replace('dl_towk_exp_ss',data.get('dl_towk_exp_ss')) if not data.get('dl_towk_exp_ss')==None \
-                            else para.text.replace('dl_towk_exp_ss','NA') #exp_grade
-                    para.text = para.text.replace('dl_towk_rec_ss',data.get('dl_towk_rec_ss')) if not data.get('dl_wj_oc_ss_grade')==None \
-                            else para.text.replace('dl_towk_rec_ss','NA') #rec_grade
-                    #CTOPP-2
-                    para.text = para.text.replace('dl_ctopp_el_ss',data.get('dl_ctopp_el_ss')) if not data.get('dl_ctopp_el_ss')==None \
-                            else para.text.replace('dl_ctopp_el_ss','NA') #el_ss
-                    para.text = para.text.replace('dl_ctopp_nr_ss',data.get('dl_ctopp_nr_ss')) if not data.get('dl_ctopp_nr_ss')==None \
-                            else para.text.replace('dl_ctopp_nr_ss','NA') #nr_ss
-                    #WISC-V
-                    para.text = para.text.replace('dl_wiscds_ss',data.get('dl_wiscds_ss')) if not data.get('dl_wiscds_ss')==None \
-                            else para.text.replace('dl_wiscds_ss','NA') #ss
+    parser = argparse.ArgumentParser(prog='DecLearn', description="Document Generator")
+    parser.add_argument("-k","--key", dest='redcap_key', default=None, help='Redcap API Token')
+    parser.add_argument("-r","--replace", dest='replacement',default=None, help='Combination of  \
+                        words to be replaced followed by redcap field name')
+    parser.add_argument("-s", "--same", dest ='same',default=None, action="store_true",\
+                        help='Bool for if replacement word and redcap field have same string')
+    parser.add_argument("-f","--file", dest='file',default=None, help='Path to template document')
+    parser.add_argument("-p","--participant", dest='participant_id', default=None,\
+                                help='Participant Id')
+    parser.add_argument("-d","--dest", dest='destination', default=None, \
+                        help='Destination for output files')
+    return parser
 
-    return document
+def run_document_generator():
+    """
+    Execution point of Document Generator
 
+    :return: None
+    """
+    parser = add_to_parser()
+    OPTIONS = parser.parse_args()
+    project = redcap_project_access(OPTIONS.redcap_key)
+    participant_filter(project,OPTIONS)
 
 if __name__ == '__main__':
-    docx = document_template_access('/home/local/VANDERBILT/ramadak/Downloads/','DecLearn Testing Report - Identified w fields.docx')
-    data = redcap_field_fetch(fields)
-    docx = replace_demography(docx, data[5])
-    docx = replace_sex(docx, data[5])
-    #print (data[5].get('dl_wj_lwid_ss_grade'))
-    docx = replace_table_contents(docx,data[5])
-    docx.save('test1.docx')
+    run_document_generator()
