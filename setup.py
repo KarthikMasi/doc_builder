57fa801d56f86347de527c4beb5b0a7a5ed4d1d3
ramadak <ramadak@masi-2.ds.vanderbilt.edu>
2018-04-09 13:08:46 -0500

    Example updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redcap_report_generator',
-    version='0.1.0',
-    scripts=['redcap_report_generate.py'],
+    version='0.1.1',
+    scripts=['redcap_report_generate'],
     install_requires=['PyCap','python-docx','argparse']
 )
