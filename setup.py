2791f62baaefa2c619e4f7977f5c2d051431c826
ramadak <ramadak@masi-26.ds.vanderbilt.edu>
2018-04-24 10:42:40 -0500

    Import file to REDCap enabled

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redcap_report_generator',
-    version='0.1.1',
+    version='0.2.0',
     scripts=['redcap_report_generate'],
     install_requires=['PyCap','python-docx','argparse']
 )
