9960ed33091208b2ffb7f9b04e5d80d8608095a8
ramadak <ramadak@masi-2.ds.vanderbilt.edu>
2018-08-16 15:21:02 -0500

    setup.py updated to 0.2.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redcap_report_generator',
-    version='0.2.6',
+    version='0.2.7',
     scripts=['redcap_report_generate'],
     install_requires=['PyCap','python-docx','argparse']
 )
